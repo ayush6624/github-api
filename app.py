fa4c2dcd8380fbb0d1c92e081b7b1ff133b767c6
ayush6624 <ayushg1214@gmail.com>
2019-11-22 01:23:13 +0530

    Add: Top 5 contributors

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, jsonify, make_response
-import json
+from flask import Flask, jsonify
+import requests
 
 
 app = Flask(__name__)
@@ -7,8 +7,33 @@ app = Flask(__name__)
 
 @app.errorhandler(404)
 def not_found(error):
-    return jsonify({'error': 'Not found'}), 404
+    return jsonify({'error': 'Invalid Request'}), 404
+
+
+@app.route('/contributor/<string:owner>/<string:repo>')
+def top_contributor(owner, repo):
+    '''
+    Lists the top 5 contributors of a repository
+
+    :param owner name
+    :param repository name
+    '''
+    req_top_contributor = requests.get(
+        'https://api.github.com/repos/' + owner + '/' + repo + '/contributors')
+
+    top_c = req_top_contributor.json()
+    top_c = top_c[:5]
+
+    for user in top_c:
+        name = user['login']
+        number_of_contrib = user['contributions']
+        url = 'https://github.com/'+name
+        user.clear()
+        user['login'] = name
+        user['contributions'] = number_of_contrib
+        user['url'] = url
+    return jsonify(top_c)
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', threaded=True)
+    app.run(debug="true", host='0.0.0.0', threaded="true")
